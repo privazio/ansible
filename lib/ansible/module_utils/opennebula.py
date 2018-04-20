2073516cdbd2d8875c511dfd446f07f63e40ffa1
Rafael <rafael@celpax.com>
2018-04-20 22:04:59 +0200

    moved from stable-2.5

---
+++ lib/ansible/module_utils/opennebula.py
@@ -0,0 +1,174 @@
+#!/usr/bin/python
+#
+# Copyright 2018 www.privaz.io Valletech AB
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+
+from os import environ
+from ssl import _create_unverified_context
+from six import string_types
+
+HAS_PYONE = True
+
+try:
+    import pyone
+except ImportError:
+    HAS_PYONE = False
+
+OPENNEBULA_COMMON_ARGS = dict(
+    endpoint = dict(type='str'),
+    session = dict(type='str', no_log=True),
+    validate_certs=dict(default=True, type='bool'),
+)
+
+
+def create_one_server(module):
+    """
+    Creates an XMLPRC client to OpenNebula.
+    Args:
+        module:
+
+    Returns:
+
+    """
+    endpoint = module.params.get("endpoint", environ.get("PYONE_ENDPOINT",False))
+    session = module.params.get("session", environ.get("PYONE_SESSION",False))
+
+    # Check if the module can run
+    if not HAS_PYONE:
+        module.fail_json(msg="pyone is required for this module")
+
+    if not endpoint:
+        module.fail_json(msg= "Either endpoint or the environment variable PYONE_ENDPOINT must be provided")
+
+    if not session:
+        module.fail_json(msg= "Either session or the environment vairable PYONE_SESSION must be provided")
+
+    if not module.params.get("validate_certs") and not "PYTHONHTTPSVERIFY" in environ:
+        return pyone.OneServer(endpoint, session=session, context=_create_unverified_context())
+    else:
+        return pyone.OneServer(endpoint,session)
+
+
+def get_host_by_name(one, name):
+    '''
+    Returns a host given its name.
+    Args:
+        one: the XMLRPC client object
+        name: the name of the host
+
+    Returns: the host object or None if the host is absent.
+
+    '''
+    hosts = one.hostpool.info()
+    for h in hosts.HOST:
+        if h.NAME == name:
+            return h
+    return None
+
+def get_cluster_by_name(one, name):
+    '''
+    Returns a cluster given its name.
+    Args:
+        one: the XMLRPC client object
+        name: the name of the cluster
+
+    Returns: the cluster object or None if the host is absent.
+
+    '''
+    clusters = one.clusterpool.info()
+    for c in clusters.CLUSTER:
+        if c.NAME == name:
+            return c
+    return None
+
+
+def get_template_by_name(one, name):
+    '''
+    Returns a template given its name.
+    Args:
+        one: the XMLRPC client object
+        name: the name of the template
+
+    Returns: the template object or None if the host is absent.
+
+    '''
+    templates = one.templatepool.info()
+    for t in templates.TEMPLATE:
+        if t.NAME == name:
+            return t
+    return None
+
+
+def cast_template(template):
+    """
+    OpenNebula handles all template types as strings
+    At some point there is a cast being performed on types provided by the user
+    This function mimics that transformation so that required template updates are detected properly
+    additionally an array will be converted to a comma separated list,
+    which works for labels and hopefully for something more.
+
+    Args:
+        template: the template to transform
+
+    Returns: the transformed template with data casts applied.
+    """
+
+    # TODO: check formally available data types in templates
+
+    for key in template:
+        value = template[key]
+        if isinstance(value, dict):
+            cast_template(template[key])
+        elif isinstance(value, list):
+            template[key] = ', '.join(value)
+        elif not isinstance(value, string_types):
+            template[key] = str(value)
+
+
+def requires_template_update(current, desired):
+    """
+    This function will help decide if a template update is required or not
+    If a desired key is missing from the current dictionary an update is required
+    If the intersection of both dictionaries is not deep equal, an update is required
+    Args:
+        current: current template as a dictionary
+        desired: desired template as a dictionary
+
+    Returns: True if a template update is required
+    """
+
+    if not desired:
+        return False
+
+    cast_template(desired)
+    intersection = dict()
+    for dkey in desired.keys():
+        if dkey in current.keys():
+            intersection[dkey] = current[dkey]
+        else:
+            return True
+    return not (desired == intersection)
+
+
+def resolve_parameters(one, module):
+    '''
+    This function resolves parameters provided by a secondary ID to the primary ID.
+    For example if cluster_name is present, cluster_id will be introduced by performing
+    the required resolution
+    Args:
+        module: the module to get the parameters from
+
+    Returns: a copy of the paramters that includes the resolved parameters.
+
+    '''
+
+    resolved_params = dict(module.params)
+
+    if 'cluster_name' in module.params:
+        clusters = one.clusterpool.info()
+        for cluster in clusters.CLUSTER:
+            if cluster.NAME == module.params.get('cluster_name'):
+                resolved_params['cluster_id'] = cluster.ID
+
+    return resolved_params
---
+++ lib/ansible/modules/cloud/opennebula/opennebula_host.py
@@ -0,0 +1,250 @@
+#!/usr/bin/python
+#
+# Copyright 2018 www.privaz.io Valletech AB
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+ANSIBLE_METADATA = {
+    'metadata_version': '1.1',
+    'status': ['preview'],
+    'supported_by': 'community'
+}
+
+DOCUMENTATION = '''
+---
+module: opennebula_host
+
+short_description: Manages OpenNebula Hosts
+
+version_added: "2.5"
+
+description:
+    - "Manages OpenNebula Hosts"
+
+options:
+    name:
+        description:
+            - Hostname of the machine to manage.
+        required: true
+    state:
+        description:
+            - Takes the host to the desired lifecycle state.
+            - If C(absent) the host will be deleted from the cluster.
+            - If C(present) the host will be created in the cluster (includes C(enabled), C(disabled) and C(offline) states).
+            - If C(enabled) the host is fully operational.
+            - C(disabled), e.g. to perform maintenance operations.
+            - C(offline), host is totally offline.
+        choices:
+            - absent
+            - present
+            - enabled
+            - disabled
+            - offline
+        default: present
+    im_mad_name:
+        description:
+            - The name of the information manager, this values are taken from the oned.conf with the tag name IM_MAD (name)
+        default: kvm
+    vmm_mad_name:
+        description:
+            - The name of the virtual machine manager mad name, this values are taken from the oned.conf with the tag name VM_MAD (name)
+        default: kvm
+    cluster_id:
+        description:
+            - The cluster ID. If it is -1, the default one will be used.
+        default: -1
+    cluster_name:
+        description:
+            - The cluster specified by name.
+    template:
+        description:
+            - The template changes to merge into the host template.
+
+extends_documentation_fragment: opennebula
+
+author:
+    - Rafael del Valle (@rvalle)
+'''
+
+EXAMPLES = '''
+- name: Create a new host in OpenNebula
+  opennebula_host:
+    name: host1
+    cluster_id: 1
+    endpoint: http://127.0.0.1:2633/RPC2
+
+- name: Create a host and adjust its template
+  opennebula_host:
+    name: host2
+    cluster_name: default
+    template:
+        LABELS:
+            - gold
+            - ssd
+        RESERVED_CPU: -100
+'''
+
+# TODO: what makes most sense to return?
+RETURN = '''
+'''
+
+# TODO: Documentation on valid state transitions is required to properly implement all valid cases
+# TODO: To be coherent with CLI this module should also provide "flush" functionality
+
+from ansible.module_utils.opennebula import OPENNEBULA_COMMON_ARGS, create_one_server, get_host_by_name, requires_template_update, resolve_parameters
+from ansible.module_utils.basic import AnsibleModule
+
+# Host State Constants, for request change and reported
+
+try:
+    from pyone import OneException, HOST_STATES, HOST_STATUS
+except ImportError:
+    OneException = Exception  #handled at module utils
+
+
+HOST_ABSENT = -99  # the host is absent (special case defined by this module)
+
+
+def allocate_host(one, module, resolved_params, result):
+    if not one.host.allocate(resolved_params.get('name'), resolved_params.get('vmm_mad_name'), resolved_params.get('im_mad_name'), resolved_params.get('cluster_id')):
+        module.fail_json(msg="could not allocate host")
+    else:
+        result['changed']= True
+    return True
+
+
+def run_module():
+    # define the available arguments/parameters that a user can pass to
+    # the module
+    module_args = OPENNEBULA_COMMON_ARGS
+    module_args.update(dict(
+        name=dict(type='str', required=True),
+        state=dict(choices=['present', 'absent', 'enabled', 'disabled', 'offline'], default='present'),
+        im_mad_name=dict(type='str', default="kvm"),
+        vmm_mad_name=dict(type='str', default="kvm"),
+        cluster_id=dict(type='int', default=-1),
+        cluster_name=dict(type='str'),
+        template = dict(type='dict'),
+    ))
+
+    # seed the result dict in the object
+    result = dict(
+        changed=False,
+        original_message='',
+        message=''
+    )
+
+    # the AnsibleModule object
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=False,
+        mutually_exclusive=[
+            ['cluster_id', 'cluster_name']
+        ]
+    )
+
+    try:
+
+        # Create the opennebula XMLRPC client
+        one = create_one_server(module)
+
+        # Resolve parameters using secondary IDs
+        resolved_params = resolve_parameters(one, module)
+
+        # Get the list of hots
+        host_name = resolved_params.get("name")
+        host = get_host_by_name(one, host_name)
+
+        # manage host state
+        desired_state = resolved_params.get('state')
+        if bool(host):
+            current_state = host.STATE
+            current_state_name = HOST_STATES(host.STATE).name
+        else:
+            current_state = HOST_ABSENT
+            current_state_name = "ABSENT"
+
+        # apply properties
+        if desired_state == 'present':
+            if current_state == HOST_ABSENT:
+                allocate_host(one, module, resolved_params, result)
+                host = get_host_by_name(one, host_name)
+
+        elif desired_state == 'enabled':
+            if current_state == HOST_ABSENT:
+                allocate_host(one, module, resolved_params, result)
+                host = get_host_by_name(one, host_name)
+            elif current_state in [HOST_STATES.DISABLED, HOST_STATES.OFFLINE]:
+                if one.host.status(host.ID, HOST_STATUS.ENABLED):
+                    result['changed'] = True
+                else:
+                    module.fail_json(msg="could not enable host")
+            elif current_state in [HOST_STATES.MONITORED]:
+                pass
+            else:
+                module.fail_json(msg="unknown host state %s, cowardly refusing to change state to enable" % current_state_name)
+
+        elif desired_state == 'disabled':
+            if current_state == HOST_ABSENT:
+                module.fail_json(msg='absent host cannot be put in disabled state')
+            elif current_state in [HOST_STATES.MONITORED, HOST_STATES.OFFLINE]:
+                if one.host.status(host.ID, HOST_STATUS.DISABLED):
+                    result['changed'] = True
+                else:
+                    module.fail_json(msg="could not disable host")
+            elif current_state in [HOST_STATES.DISABLED]:
+                pass
+            else:
+                module.fail_json(msg="unknown host state %s, cowardly refusing to change state to disable" % current_state_name)
+
+        elif desired_state == 'offline':
+            if current_state == HOST_ABSENT:
+                module.fail_json(msg='absent host cannot be placed in offline state')
+            elif current_state in [HOST_STATES.MONITORED, HOST_STATES.DISABLED]:
+                if one.host.status(host.ID, HOST_STATUS.OFFLINE):
+                    result['changed'] = True
+                else:
+                    module.fail_json(msg="could not set host offline")
+            elif current_state in [HOST_STATES.OFFLINE]:
+                pass
+            else:
+                module.fail_json(msg="unknown host state %s, cowardly refusing to change state to offline" % current_state_name)
+
+        elif desired_state == 'absent':
+            if current_state != HOST_ABSENT:
+                if one.host.delete(host.ID):
+                    result['changed'] = True
+                else:
+                    module.fail_json(msg="could not delete host from cluster")
+
+        # if we reach this point we can assume that the host was taken to the desired state
+
+        if desired_state != "offline":
+
+            # manipulate or modify the template
+            desired_template_changes = resolved_params.get('template')
+            if requires_template_update(host.TEMPLATE, desired_template_changes):
+                # setup the root element so that pytone will generate XML instead of attribute vector
+                desired_template_changes = {"TEMPLATE": desired_template_changes}
+                if one.host.update(host.ID, desired_template_changes, 1):  # merge the template
+                    result['changed'] = True
+                else:
+                    module.fail_json(msg="failed to update the host template")
+
+            # the cluster
+            if host.CLUSTER_ID != resolved_params.get('cluster_id'):
+                if one.cluster.addhost(resolved_params.get('cluster_id'), host.ID):
+                    result['changed'] = True
+                else:
+                    module.fail_json(msg="failed to update the host cluster")
+
+        # return
+        module.exit_json(**result)
+    except OneException as e:
+        module.fail_json(msg="OpenNebula Exception: %s" % e)
+
+
+def main():
+    run_module()
+
+if __name__ == '__main__':
+    main()
